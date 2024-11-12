a9131a31a7330420a6917f0c6264ee280e49659d
s1nno <haydensandler1@gmail.com>
2024-11-12 17:42:44 -0500

    readded about plan on revamping to include languages and personal info

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
       <Navbar />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <HeroSection />
-        <AboutSection />
+        {/* <AboutSection /> */}
         <Projects />
         <Contact />
       </main>
